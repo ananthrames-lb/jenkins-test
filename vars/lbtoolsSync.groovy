8205fc8635891cd912797292d2095627ff5ded3b
Ananthram E S <ananthram.es@liquiditybook.com>
2025-02-10 12:38:35 +0000

    test

--- vars/lbtoolsSync.groovy
+++ vars/lbtoolsSync.groovy
@@ -1,12 +1,6 @@
-def call(Map config = [:]) {
-    dir("LiquidityBook") {  
-        withAWS(region: 'us-east-1', credentials: 'terraform') {       
-            script{          
-                rabbit = config.containsKey("rabbit") ? config.rabbit : "rabbitmq.prod.dev-lqbk.com"
-                command = config.containsKey("command") ? config.command : "hotSync"
+def call() {
+    map = [:]
 
-                sh "java -jar ./target/helloworld/helloworld.jar"
-            }
-        }
-    }
+    map.command = [ name: 'command', choices: ['filesync', 'listNodes', 'hotSync', 'uninstall', 'install'], description: 'The operation to perform.\nListNodes - only shows what nodes will be deployed, but does not deploy\nhotSync - hotsyncs the nodes.' ]
+return map
 }
